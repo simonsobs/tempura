72535b02c0a0412662bf8d2f189decc7cb226b03
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2021-11-09 21:22:59 -0800

    implement asymmetric estimator noise spectrum

--- scripts/generate_interface.sh
+++ scripts/generate_interface.sh
@@ -6,9 +6,9 @@ dir_f90src="fortran/"
 dir_pytempura="pytempura/"
 
 # files to be scanned
-scan="norm_lens.f90 norm_src.f90 norm_general.f90"
+scan="norm_lens.f90 norm_src.f90 norm_general.f90 noise_spec.f90"
 
 # create python interface
 python ${dir_scripts}/interface.py -moddir ${dir_f90src} -modname ${scan}
-mv norm_lens.py norm_src.py norm_general.py ${dir_pytempura}
+mv norm_lens.py norm_src.py norm_general.py noise_spec.py ${dir_pytempura}
 
